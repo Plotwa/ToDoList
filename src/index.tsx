6ddbfee84407a098d727249742c8a90abcc1b878
Fury <johndoe@example.com>
2022-09-14 22:10:19 +0300

    updateindex

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './views/App/app';
-
+import './views/styles/common.scss'
+import './views/styles/reset.scss'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement);
 root.render(
